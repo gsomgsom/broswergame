a58ac763195ceeda358d7d22f3992462a3c24039
GsomGsom <zhelneen@yandex.ru>
2017-10-28 02:10:17 +0500

    персонаж, инвентарь

--- protected/modules/home/views/default/index.php
+++ protected/modules/home/views/default/index.php
@@ -1,13 +1,56 @@
 <h3>Персонаж</h3>
-<p>@TODO - статы, аватар и рюкзак</p>
+<div class="row">
+	<div class="col-md-6 nopadding">
+		<div style="width: 100%; padding-left: 4rem;">Сила: <span style="position: absolute; right: 4rem;"><b>64</b></span></div>
+		<div style="width: 100%; padding-left: 4rem;">Защита: <span style="position: absolute; right: 4rem;"><b>64</b></span></div>
+		<div style="width: 100%; padding-left: 4rem;">Ловкость: <span style="position: absolute; right: 4rem;"><b>64</b></span></div>
+		<div style="width: 100%; padding-left: 4rem;">Стойкость: <span style="position: absolute; right: 4rem;"><b>64</b></span></div>
+		<div style="width: 100%; padding-left: 4rem;">Интуиция: <span style="position: absolute; right: 4rem;"><b>64</b></span></div>
+		<hr>
+		<div style="width: 100%; padding-left: 4rem;">Известность: <span style="position: absolute; right: 4rem;"><b>1234</b></span></div>
+		<div style="width: 100%; padding-left: 4rem;">Карма: <span style="position: absolute; right: 4rem;"><b>+100</b></span></div>
+		<div style="width: 100%; padding-left: 4rem;">Опыт: <span style="position: absolute; right: 4rem;"><b>12 / 1000</b></span></div>
+	</div>
+	<div class="col-md-6 nopadding">
+		<div class="row nomargin">
+			<div class="col-md-2 nopadding">
+				<div class="player-items">
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Шляпа</b><br><i><small>Слот для головных уборов</small></i>"><img src="/assets/img/slot_helm64.png"></span>
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Куртка</b><br><i><small>Слот для куртки</small></i>"><img src="/assets/img/slot_shirt64.png"></span>
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Штаны</b><br><i><small>Слот для штанов</small></i>"><img src="/assets/img/slot_pants64.png"></span>
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Правая рука</b><br><i><small>Слот для оружия</small></i>"><img src="/assets/img/slot_weapon64.png"></span>
+				</div>
+			</div>
+			<div class="col-md-8 nopadding">
+				<img src="/assets/img/av-boy01.png" class="text-center">
+			</div>
+			<div class="col-md-2 nopadding">
+				<div class="player-items">
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Ожерелье</b><br><i><small>Слот для ожерелья</small></i>"><img src="/assets/img/slot_necklace64.png"></span>
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Накидка</b><br><i><small>Слот для плаща</small></i>"><img src="/assets/img/slot_cloak64.png"></span>
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Талисман</b><br><i><small>Слот для талисмана</small></i>"><img src="/assets/img/slot_trinket64.png"></span>
+					<span class="border rounded border-secondary nomargin" data-toggle="tooltip" data-html="true" title="<b>Левая рука</b><br><i><small>Слот для оружия или щита</small></i>"><img src="/assets/img/slot_shield64.png"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <h4>Рюкзак</h4>
 <div class="player-items">
-	<span class="border border-success" data-toggle="tooltip" data-html="true" title="<b>Подарок</b><br><i><small>Судя по звуку, в коробочке что-то есть. И конечно очень ценное! Открыть! Срочно!</small></i>"><img src="/assets/img/present_box_striped64.png"></span>
-	<span class="border border-primary" data-toggle="tooltip" data-html="true" title="<b>Красное зелье</b><br><i><small>Восстанавливает здоровье до максимума</small></i>"><img src="/assets/img/potion_small_red64.png"></span>
-	<span class="border border-primary" data-toggle="tooltip" data-html="true" title="<b>Синее зелье</b><br><i><small>Восстанавливает энергию до максимума</small></i>"><img src="/assets/img/potion_big_blue64.png"></span>
-	<span class="border border-dark" data-toggle="tooltip" data-html="true" title="<b>Синий свиток</b><br><i><small>Колдует что-то там. Пока не разобрались, что именно.</small></i>"><img src="/assets/img/scroll_blue64.png"></span>
-	<span class="border border-secondary" data-toggle="tooltip" data-html="true" title="<b>Ракушки</b><br><i><small>Ценятся среди тех, у уого они - дефицит</small></i>"><img src="/assets/img/shells64.png"></span>
-	<span class="border border-secondary" data-toggle="tooltip" data-html="true" title="<b>Черепушки</b><br><i><small>Победителей не судят. Но между собой победители меряются. Чем? Правильно, Черепушками.</small></i>"><img src="/assets/img/skulls64.png"></span>
-	<span class="border border-warning" data-toggle="tooltip" data-html="true" title="<b>Брильянты</b><br><i><small>Сверкушки-побрякушки. И ещё лучшие друзья девушек. Ага.</small></i>"><img src="/assets/img/diamonds64.png"></span>
-	<span class="border border-warning" data-toggle="tooltip" data-html="true" title="<b>Мёд</b><br><i><small>Сладкий, как победа над пчёлами, у которых его с таким трудом отобрали.</small></i>"><img src="/assets/img/honey64.png"></span>
+	<span class="border rounded border-success" style="background-color: #6b6;" data-toggle="tooltip" data-html="true" title="<b>Подарок</b><br><i><small>Судя по звуку, в коробочке что-то есть. И конечно очень ценное! Открыть! Срочно!</small></i>"><img src="/assets/img/present_box_striped64.png"></span>
+	<span class="border rounded border-primary" style="background-color: #66b;" data-toggle="tooltip" data-html="true" title="<b>Красное зелье</b><br><i><small>Восстанавливает здоровье до максимума</small></i>"><img src="/assets/img/potion_small_red64.png"></span>
+	<span class="border rounded border-primary" style="background-color: #66b;" data-toggle="tooltip" data-html="true" title="<b>Синее зелье</b><br><i><small>Восстанавливает энергию до максимума</small></i>"><img src="/assets/img/potion_big_blue64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #ddd;" data-toggle="tooltip" data-html="true" title="<b>Синий свиток</b><br><i><small>Колдует что-то там. Пока не разобрались, что именно.</small></i>"><img src="/assets/img/scroll_blue64.png"></span>
+	<span class="border rounded border-secondary" style="background-color: #ccc;" data-toggle="tooltip" data-html="true" title="<b>Ракушки</b><br><i><small>Ценятся среди тех, у уого они - дефицит</small></i>"><img src="/assets/img/shells64.png"></span>
+	<span class="border rounded border-secondary" style="background-color: #ccc;" data-toggle="tooltip" data-html="true" title="<b>Черепушки</b><br><i><small>Победителей не судят. Но между собой победители меряются. Чем? Правильно, Черепушками.</small></i>"><img src="/assets/img/skulls64.png"></span>
+	<span class="border rounded border-warning" style="background-color: #bb6;" data-toggle="tooltip" data-html="true" title="<b>Брильянты</b><br><i><small>Сверкушки-побрякушки. И ещё лучшие друзья девушек. Ага.</small></i>"><img src="/assets/img/diamonds64.png"></span>
+	<span class="border rounded border-warning" style="background-color: #bb6;" data-toggle="tooltip" data-html="true" title="<b>Мёд</b><br><i><small>Сладкий, как победа над пчёлами, у которых его с таким трудом отобрали.</small></i>"><img src="/assets/img/honey64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
+	<span class="border rounded border-dark" style="background-color: #bbb;"><img src="/assets/img/empty64.png"></span>
 </div>
